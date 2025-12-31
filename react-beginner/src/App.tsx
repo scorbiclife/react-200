1c288450df53a307846c22357e6136e099b9013b
scorbiclife <scorbiclife+git@gmail.com>
2025-12-31 14:33:00 +0900

    feat: setup project

---
+++ react-beginner/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import R003_ImportComponent from "./R003_ImportComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <p>Apply HTML</p>
+      <R003_ImportComponent></R003_ImportComponent>
+    </div>
+  )
+}
+
+export default App;
---
+++ react-beginner/src/R003_ImportComponent.tsx
@@ -0,0 +1,5 @@
+function R003_ImportComponent() {
+  return <h2>[This is an imported component]</h2>;
+}
+
+export default R003_ImportComponent;
