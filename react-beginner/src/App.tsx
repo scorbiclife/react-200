f73cd5ccb2372eae367af28c66ac25a06219e9c6
scorbiclife <scorbiclife+git@gmail.com>
2026-01-01 06:18:11 +0900

    feat: R030: implement a function component

--- react-beginner/src/App.tsx
+++ react-beginner/src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css"
-import R003_ImportComponent from "./R003_ImportComponent";
+import R030_FunctionComponent from "./R030_FunctionComponent";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>Apply HTML</p>
-      <R003_ImportComponent></R003_ImportComponent>
+      <R030_FunctionComponent content="[This is a function component]"></R030_FunctionComponent>
     </div>
   )
 }
---
+++ react-beginner/src/R030_FunctionComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function R030_FunctionComponent(props: { content: string }) {
+  const { content } = props;
+  return <h2>{content}</h2>
+}
+
+export default R030_FunctionComponent;
