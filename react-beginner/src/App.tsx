ada5126947246042ebc49a986e96855dd3eb0a48
scorbiclife <scorbiclife+git@gmail.com>
2026-01-02 16:43:27 +0900

    feat: R036: use reactstrap breadcrumbs

--- react-beginner/src/App.tsx
+++ react-beginner/src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import R035_ReactstrapBadges from "./R035_ReactstrapBadges";
+import R036_ReactstrapBreadcrumbs from "./R036_ReactstrapBreadcrumbs";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <R035_ReactstrapBadges></R035_ReactstrapBadges>
+      <R036_ReactstrapBreadcrumbs></R036_ReactstrapBreadcrumbs>
     </div>
   );
 }
---
+++ react-beginner/src/R036_ReactstrapBreadcrumbs.tsx
@@ -0,0 +1,21 @@
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+
+function R036_ReactstrapBreadcrumbs() {
+  return (
+    <div id="top">
+      <Breadcrumb tag="nav" listTag="div">
+        <BreadcrumbItem tag="a" href="#top">
+          Go_top
+        </BreadcrumbItem>
+        <BreadcrumbItem tag="a" href="#bottom">
+          Go_bottom
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <div id="bottom" style={{ marginTop: "1000px" }}>
+        <span>bottom</span>
+      </div>
+    </div>
+  );
+}
+
+export default R036_ReactstrapBreadcrumbs;
