00a92a87630c9b3fc6d82f58dfac0df504868436
scorbiclife <scorbiclife+git@gmail.com>
2026-01-03 10:12:36 +0900

    feat: R039: use colored buttons

--- react-beginner/src/App.tsx
+++ react-beginner/src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import R038_ReactstrapButtonGroup from "./R038_ReactstrapButtonGroup";
+import R039_ReactstrapButtons from "./R039_ReactstrapButtons";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <R038_ReactstrapButtonGroup></R038_ReactstrapButtonGroup>
+      <R039_ReactstrapButtons></R039_ReactstrapButtons>
     </div>
   );
 }
---
+++ react-beginner/src/R039_ReactstrapButtons.tsx
@@ -0,0 +1,18 @@
+import { Button, ButtonGroup } from "reactstrap";
+
+export default function R039_ReactstrapButtons() {
+  return (
+    <div style={{ padding: "0px" }}>
+      <ButtonGroup style={{ padding: "0px" }}>
+        <Button color="primary">Primary</Button>
+        <Button color="info">Info</Button>
+        <Button color="success">Success</Button>
+        <Button color="warning">Warning</Button>
+        <Button color="danger">danger</Button>
+        <Button color="dark">dark</Button>
+        <Button color="secondary">secondary</Button>
+        <Button color="light">light</Button>
+      </ButtonGroup>
+    </div>
+  );
+}
